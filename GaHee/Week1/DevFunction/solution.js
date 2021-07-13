e98928b46f7bfab059167782e8df40d10da3f254
Gahee Kim <rkgml981105@gmail.com>
2021-07-13 11:06:37 +0900

    add week1 solution and test codes

---
+++ GaHee/Week1/DevFunction/solution.js
@@ -0,0 +1,8 @@
+module.exports = function solution(numbers) {
+  const copyNum = numbers.map((num) => num.toString());
+  
+  const answer = copyNum.sort((a, b) => (b + a) - (a + b)).join('');
+  // 예외처리
+  if (answer[0] === '0') return '0';
+  return answer;
+}
